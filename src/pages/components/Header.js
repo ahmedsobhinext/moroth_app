427986528e53b327afa0975fc94e8236e9f88d1d
ahmedsobhinext <ahmedsobhinext@gmail.com>
2025-02-19 01:46:42 +0200

    Image

--- src/pages/components/Header.js
+++ src/pages/components/Header.js
@@ -1,6 +1,7 @@
 // components/Header.js
 
 import Link from 'next/link';
+import Image from 'next/image';
 
 const Header = () => {
   return (
@@ -8,7 +9,7 @@ const Header = () => {
       {/* Logo */}
       <Link href="/">
       <div className="flex items-center space-x-4">
-        <img src="logo.png" alt="Logo" className="h-16 w-auto" />
+        <Image src="/logo.png" alt="Logo" className="h-16 w-auto" />
         {/* <span className="text-2xl font-bold text-green-700">MOROTH</span> */}
       </div>
       </Link>
